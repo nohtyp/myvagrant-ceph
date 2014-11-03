a273295fc7e9374d1ca889ba4748da815fee0ee9
nohtyp <thomas.foster80@gmail.com>
2014-11-03 14:59:46 -0500

    created a puppet fact for available disks

--- facts/available_devices.rb
+++ facts/available_devices.rb
@@ -1,17 +1,12 @@
 require 'facter'
 #
-#
-def mydevice(device)
-  num = 0
-  is_used = Facter::Util::Resolution.exec('mount').scan("#{device}")
-  return false if is_used.empty?
-  num += 1
-  Facter.add("unused_disk#{num}") do
-    setcode do
-      device
+Facter::Util::Resolution.exec('lsblk -n -d -e 11,1').scan(/(sd\D+)/).each do |disks|
+  is_used = Facter::Util::Resolution.exec('mount').scan("#{disks}".strip)
+  if is_used.empty?
+    Facter.add("unused_disk_#{disks}".strip) do
+      setcode do
+        disks
+      end
     end
   end
 end
-
-disks = Facter::Util::Resolution.exec('lsblk -n -d -e 11,1').scan(/(sd\D+)/)
-disks.each { |x| mydevice(x) }
