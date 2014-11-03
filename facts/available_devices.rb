c5ac89f5618312fda265baf5a76e3bccce8ca248
nohtyp <thomas.foster80@gmail.com>
2014-11-03 13:28:58 -0500

    modified to use guard clause from rubocop

--- facts/available_devices.rb
+++ facts/available_devices.rb
@@ -4,12 +4,11 @@ require 'facter'
 def mydevice(device)
   num = 0
   is_used = Facter::Util::Resolution.exec('mount').scan("#{device}")
-  if is_used.empty? 
-    num += 1
-    Facter.add("unused_disk#{num}") do
-      setcode do
-        device
-      end
+  return false if is_used.empty?
+  num += 1
+  Facter.add("unused_disk#{num}") do
+    setcode do
+      device
     end
   end
 end
