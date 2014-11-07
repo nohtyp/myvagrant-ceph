56400c5aea0076c97770acad0c3b3db9a247ca2e
nohtyp <thomas.foster80@gmail.com>
2014-11-07 09:51:50 -0500

    modified fact to run command only once

--- facts/available_devices.rb
+++ facts/available_devices.rb
@@ -1,12 +1,18 @@
 require 'facter'
-#
+#This fact will get back all unused disks to use for provisioning a ceph node.
+
 Facter::Util::Resolution.exec('lsblk -n -d -e 11,1').scan(/sd\D+/).each do |disk|
   disk = disk.strip!
-  in_use = Facter::Util::Resolution.exec('mount').include?(disk)
-  if !in_use
-    Facter.add("unused_disk_#{disk}") do
-      setcode do
-        disk
+  if disk.nil?
+  else
+    in_use = Facter::Util::Resolution.exec('mount')
+    disk.each do |not_found|
+      if !in_use.include?(not_found)
+        Facter.add("unused_disk_#{disk}") do
+          setcode do
+            disk
+          end
+        end
       end
     end
   end
